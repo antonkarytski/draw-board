e37cac2a91a507019a7cbc7fb186242e4c454fa5
Anon Karytski <heyheyjude23@gmail.com>
2021-09-18 10:55:11 +0300

    feat: add step forward button refactor: separate repeater logic from RepeaterBoard component to useDrawBoardHistory hook

--- src/components/drawBoard/RepeaterBoard.tsx
+++ src/components/drawBoard/RepeaterBoard.tsx
@@ -1,67 +1,19 @@
-import React, { MutableRefObject, useRef } from "react";
+import React, { MutableRefObject } from "react";
 import { PointsHistory, useDrawBoard } from "./hook.drawBoard";
 import classes from "./styles.module.scss";
 import PlayButton from "../buttons/PlayButton";
-import { findLastIndex } from "../../lib/findLastIndex";
+import { useDrawBoardHistory } from "./hook.drawBoardHistory";
 
 type RepeaterBoardProps = {
   history: MutableRefObject<PointsHistory>;
 };
 
 export default function RepeaterBoard({ history }: RepeaterBoardProps) {
-  const { canvasRef, drawDot, drawLine, resetPosition, clear } = useDrawBoard();
-  const drawIndex = useRef(0);
-  const isPlaying = useRef(false);
-
-  function withTimer() {
-    if (drawIndex.current >= history.current.length) {
-      drawIndex.current = 0;
-      return;
-    }
-    const { firstClick, ...point } = history.current[drawIndex.current];
-    if (firstClick) {
-      resetPosition();
-      drawDot(point);
-    } else {
-      drawLine(point);
-    }
-    if (drawIndex.current === history.current.length - 1) return;
-    drawIndex.current = drawIndex.current + 1;
-    if (!isPlaying.current) return;
-    const nextDelay =
-      history.current[drawIndex.current].timeStamp - point.timeStamp;
-    const timer = setTimeout(
-      () => {
-        withTimer();
-        clearTimeout(timer);
-      },
-      nextDelay > 500 ? 500 : nextDelay
-    );
-  }
-
-  function stepBack() {
-    clear();
-    if (!drawIndex.current) return;
-    const prevStepIndex = findLastIndex(
-      history.current,
-      ({ firstClick }, index) => {
-        return !!firstClick && index < drawIndex.current;
-      }
-    );
-    drawIndex.current = prevStepIndex;
-    history.current
-      .filter((_, index) => {
-        return index <= prevStepIndex;
-      })
-      .forEach(({ firstClick, ...point }) => {
-        if (firstClick) {
-          resetPosition();
-          drawDot(point);
-        } else {
-          drawLine(point);
-        }
-      });
-  }
+  const { canvasRef, ...controller } = useDrawBoard();
+  const { stepBack, stepForward, togglePlaying } = useDrawBoardHistory({
+    controller,
+    history,
+  });
 
   return (
     <div>
@@ -72,13 +24,8 @@ export default function RepeaterBoard({ history }: RepeaterBoardProps) {
         className={classes.Canvas}
       />
       <button onClick={stepBack}>Step back</button>
-      <PlayButton
-        onClick={() => {
-          isPlaying.current = !isPlaying.current;
-          if (isPlaying.current) withTimer();
-          //history.current.splice(0, history.current.length);
-        }}
-      />
+      <PlayButton onClick={togglePlaying} />
+      <button onClick={stepForward}>Step forward</button>
     </div>
   );
 }
--- src/components/drawBoard/hook.drawBoardHistory.ts
+++ src/components/drawBoard/hook.drawBoardHistory.ts
@@ -1,1 +1,95 @@
-export function useDrawBoardHistory() {}
+import { MutableRefObject, useRef } from "react";
+import { PointsHistory, useDrawBoard } from "./hook.drawBoard";
+import { findLastIndex } from "../../lib/findLastIndex";
+
+type UseDrawBoardHistoryProps = {
+  history: MutableRefObject<PointsHistory>;
+  controller: Omit<ReturnType<typeof useDrawBoard>, "canvasRef">;
+};
+
+export function useDrawBoardHistory({
+  history,
+  controller,
+}: UseDrawBoardHistoryProps) {
+  const drawIndex = useRef(0);
+  const isPlaying = useRef(false);
+  const { drawDot, drawLine, resetPosition, clear } = controller;
+
+  function withTimer() {
+    if (drawIndex.current >= history.current.length) {
+      drawIndex.current = 0;
+      return;
+    }
+    const { firstClick, ...point } = history.current[drawIndex.current];
+    if (firstClick) {
+      resetPosition();
+      drawDot(point);
+    } else {
+      drawLine(point);
+    }
+    if (drawIndex.current === history.current.length - 1) return;
+    drawIndex.current = drawIndex.current + 1;
+    if (!isPlaying.current) return;
+    const nextDelay =
+      history.current[drawIndex.current].timeStamp - point.timeStamp;
+    const timer = setTimeout(
+      () => {
+        withTimer();
+        clearTimeout(timer);
+      },
+      nextDelay > 500 ? 500 : nextDelay
+    );
+  }
+
+  function stepBack() {
+    clear();
+    if (!drawIndex.current) return;
+    const prevStepIndex = findLastIndex(
+      history.current,
+      ({ firstClick }, index) => {
+        return !!firstClick && index < drawIndex.current;
+      }
+    );
+    drawIndex.current = prevStepIndex;
+    [...history.current]
+      .slice(0, prevStepIndex + 1)
+      .forEach(({ firstClick, ...point }) => {
+        if (firstClick) {
+          resetPosition();
+          drawDot(point);
+        } else {
+          drawLine(point);
+        }
+      });
+  }
+
+  function stepForward() {
+    if (drawIndex.current >= history.current.length - 1) return;
+    const nextStepIndex =
+      history.current.findIndex(({ firstClick }, index) => {
+        return !!firstClick && index > drawIndex.current;
+      }) || history.current.length - 1;
+    [...history.current]
+      .slice(drawIndex.current + 1, nextStepIndex + 1)
+      .forEach(({ firstClick, ...point }) => {
+        if (firstClick) {
+          resetPosition();
+          drawDot(point);
+        } else {
+          drawLine(point);
+        }
+      });
+    drawIndex.current = nextStepIndex;
+  }
+
+  function togglePlaying() {
+    isPlaying.current = !isPlaying.current;
+    if (isPlaying.current) withTimer();
+  }
+
+  return {
+    togglePlaying,
+    stepBack,
+    stepForward,
+  };
+}
